b1e869a843a4d6d88970ce48f39b6039ab45241a
erikkjernlie <erik.kjernlie@gmail.com>
2021-10-02 11:26:37 +0200

    Add story of game

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -2,24 +2,38 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import { Bubbles } from "@gobistories/gobi-web-integration";
 import Container from "./Container/Container";
+import animation from "public/static/animations/confetti.json";
+import Lottie from "lottie-react";
 
 const Story = () => {
-
-        useEffect(() => {
-           if (typeof window !== undefined) {
-            new Bubbles({
-                container: "#container",
-                color: "red",
-                stories: [{ id: "xj4a9", title: "Halla" }],
-                bubbleSize: "500vw",
-              });
-           }
-        }, [window])
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      new Bubbles({
+        container: "#container",
+        color: "#007EA8",
+        stories: [{ id: "xj4a9", title: "Kjære Kasper 💛🥏🌟" }],
+        bubbleSize: "250px",
+      });
+    }
+  }, [window]);
   return (
-    <Container center style={{
+    <Container
+      center
+      style={{
         width: "100vw",
-        height: "100vh"
-    }}>
+        height: "100vh",
+        backgroundColor: "#BEE3E5",
+      }}
+    >
+      <Lottie
+        animationData={animation}
+        style={{
+          width: "100vw",
+          height: "100vw",
+          position: "absolute",
+          zIndex: 0,
+        }}
+      />
       <div id="container"></div>
     </Container>
   );
